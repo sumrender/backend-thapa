cbe7103fdc7f8062796d51974ca4ec14a19ac73f
sumrender <sumrenders@gmail.com>
2022-12-15 12:28:16 +0530

    add product schema

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connectDB");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 const product_routes = require("./routes/products");
--- db/connectDB.js
+++ db/connectDB.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const uri = "mongodb://127.0.0.1:27017/backendThapa";
+// "mongodb://127.0.0.1:27017/backendThapa";
 // "mongodb://localhost:27017/backendThapa"
 // uri with localhost does not work
 
 function connectDB() {
-  return mongoose.connect(uri, {
+  return mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
